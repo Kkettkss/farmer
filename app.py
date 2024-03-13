376b16eab38e3112a032bed49680a8531db19043
Kkettkss <115526547+Kkettkss@users.noreply.github.com>
2024-03-13 20:04:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 # app.config['SQLALCHEMY_DATABASE_URL']='mysql://username:password@localhost/database_name'
-app.config['SQLALCHEMY_DATABASE_URI']='mysql://farmer:Admin123@farmerm.mysql.database.azure.com/farmer'
+app.config['SQLALCHEMY_DATABASE_URI']='mysql://Farmer:Admin123@farmerdata.mysql.database.azure.com/Farmer'
 db=SQLAlchemy(app)
 
 # here we will create db models that is tables
